af11e35afde5a94bb928949dcd132ddd533f2505
Chris Kim <oats87g@gmail.com>
2020-03-06 15:01:18 -0800

    add glibc-static

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -2,7 +2,7 @@
 set -e
 
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv
+dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv glibc-static
 
 mkdir -p bin dist
 if [ -e ./scripts/$1 ]; then
