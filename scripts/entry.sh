ce935e2ec769504ff1bd649f0c54265ca97f5a2f
Chris Kim <oats87g@gmail.com>
2020-03-06 13:48:31 -0800

    more hax

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv
+
 mkdir -p bin dist
 if [ -e ./scripts/$1 ]; then
     ./scripts/"$@"
