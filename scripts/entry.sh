76c863f8c9a037675e04a9cfdeb7479b2ac9a669
Chris Kim <oats87g@gmail.com>
2020-03-09 10:41:20 -0700

    try to compile from source the static library

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -2,7 +2,15 @@
 set -e
 
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv glibc-static sqlite sqlite-libs
+dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv glibc-static sqlite sqlite-libs gcc rpm-build
+
+dnf download --source sqlite-libs
+rpm -ivh sqlite-*src.rpm
+rpmbuild -bp ~/rpmbuild/SPECS/sqlite.spec
+cd ~/rpmbuild/BUILD/sqlite-src-*
+./configure
+make
+cp .libs/libsqlite3.a /usr/lib64
 
 mkdir -p bin dist
 if [ -e ./scripts/$1 ]; then
