f9c9d461c8cbf2fd5e1053797b61b4ac08dccd96
Chris Kim <oats87g@gmail.com>
2020-03-09 08:34:14 -0700

    adding sqlite and sqlite-libs

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -2,7 +2,7 @@
 set -e
 
 dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv glibc-static
+dnf install --nobest -y docker-ce jq python2 libffi-devel libseccomp-devel libuv glibc-static sqlite sqlite-libs
 
 mkdir -p bin dist
 if [ -e ./scripts/$1 ]; then
